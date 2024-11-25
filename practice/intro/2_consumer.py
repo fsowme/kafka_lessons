9ec04a5f1d3c4313ea3d8687b0f1144f9538b756
Vitaly Mikhailov <vmikhailov@reksoft.ru>
2024-11-25 23:08:59 +0300

    first consumer (python)

---
+++ practice/intro/2_consumer.py
@@ -0,0 +1,33 @@
+from confluent_kafka import Consumer
+
+
+# Настройка консьюмера – адрес сервера
+conf = {
+    "bootstrap.servers": "localhost:9095",
+    "group.id": "my-group_2",
+    "auto.offset.reset": "earliest",
+}
+# Создание консьюмера
+consumer = Consumer(conf)
+
+# Подписка на топик
+consumer.subscribe(["some-topic"])
+
+# Чтение сообщений в бесконечном цикле
+try:
+    while True:
+        # Получение сообщений
+        msg = consumer.poll(0.1)
+
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"Ошибка: {msg.error()}")
+            continue
+
+        key = msg.key().decode("utf-8")
+        value = msg.value().decode("utf-8")
+        print(f"Получено сообщение: {key=}, {value=}, offset={msg.offset()}")
+finally:
+    # Закрытие консьюмера
+    consumer.close()
