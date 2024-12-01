8afd59d7b91ef6b5dd29716573c6492dd8929d05
Vitaly Mikhailov <vmikhailov@reksoft.ru>
2024-12-01 19:41:38 +0300

    replication

--- practice/intro/3_replicated_topic.py
+++ practice/intro/3_replicated_topic.py
@@ -3,13 +3,15 @@ from confluent_kafka.admin import NewTopic, AdminClient
 admin = AdminClient({'bootstrap.servers': 'localhost:9095'})
 
 replicated_topic = NewTopic(
-    topic="replicated_topic_qwe",
+    topic="replicated_topic_qwessss",
     num_partitions=5,
     replication_factor=2
 )
 
 try:
-    admin.create_topics([replicated_topic])
+    res = admin.create_topics(new_topics=[replicated_topic], validate_only=False)
+    for topic, r in res.items():
+        r.result()
     print("Topic created successfully")
 except Exception as error:
     print(error)
