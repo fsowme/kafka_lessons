1c0eb3c47c36eb3a546b045328f8ab4d3a1dca8b
Vitaly Mikhailov <vmikhailov@reksoft.ru>
2024-12-09 23:29:08 +0300

    practice kafka stream

---
+++ practice/2_module/1_stream_processing/consumer.py
@@ -0,0 +1,14 @@
+from common import Config, ConsumeJob
+
+CONFIG = Config(
+    group__id='consumer_pull',
+    auto__offset__reset='earliest',
+    enable__auto__commit=False,
+    bootstrap__servers='localhost:9094'
+)
+TOPIC = 'output-topic'
+
+
+with ConsumeJob(CONFIG) as job:
+    for message in job.run(TOPIC, 0.5):
+        print(message)
