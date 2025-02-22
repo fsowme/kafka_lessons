d179ba8396258ab93868a6372889d5de51c3a0e2
Vitaly Mikhailov <vmikhailov@reksoft.ru>
2025-02-22 21:50:50 +0300

    security

--- practice/4_module/security/producer.py
+++ practice/4_module/security/producer.py
@@ -1,4 +1,3 @@
-import time
 import uuid
 
 from confluent_kafka import Producer
@@ -7,21 +6,26 @@ if __name__ == "__main__":
     producer_conf = {
         "bootstrap.servers": "localhost:9093",
 
-        "security.protocol": "SSL",
+        # Настройки безопасности SSL
+        "security.protocol": "SASL_PLAINTEXT",
         "ssl.ca.location": "ca.crt",  # Сертификат центра сертификации
         "ssl.certificate.location": "kafka-1-creds/kafka-1.crt",  # Сертификат клиента Kafka
         "ssl.key.location": "kafka-1-creds/kafka-1.key",  # Приватный ключ для клиента Kafka
+
+        # Настройки SASL-аутентификации
+        "sasl.mechanism": "PLAIN",  # Используемый механизм SASL (PLAIN)
+        "sasl.username": "admin",  # Имя пользователя для аутентификации
+        "sasl.password": "admin-secret",  # Пароль пользователя для аутентификации
     }
 
     producer = Producer(producer_conf)
     while True:
         key = f"key-{uuid.uuid4()}"
-        value = "SSL message"
+        value = "SASL/PLAIN"
         producer.produce(
-            "ssl-topic",
+            "sasl-plain-topic",
             key=key,
             value=value,
         )
         producer.flush()
         print(f"Отправлено сообщение: {key=}, {value=}")
-        time.sleep(1)
